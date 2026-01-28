33e84dfa532209b938fbc80e5ff3b1ec1b915df6
Dave Dawson <davedawson.co@gmail.com>
2026-01-28 17:30:31 +1100

    Update index.html, package.json, and package-lock.json to include Vite plugin and improve HTML structure
    - Reformatted index.html for better readability.
    - Added vite-plugin-radar dependency to package.json and package-lock.json.
    - Configured Vite to use the radar plugin for analytics in vite.config.ts.

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePluginRadar } from 'vite-plugin-radar'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePluginRadar({
+      analytics: {
+        id: 'G-COMING_SOON',
+      },
+    })
+  ],
+  base: "/",
+  server: {
+    open: true
+  },
 })
-
